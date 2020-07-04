10ec2aca733932632b4f177bba5ccf992f004abb
Exo-Byte <47266822+Exo-Byte@users.noreply.github.com>
2020-07-04 16:17:25 -0700

    Create StatusPageStatus.lua

---
+++ src/StatusPageStatus.lua
@@ -0,0 +1,12 @@
+local StatusPageStatus = {
+	
+	Operational = "operational";
+	UnderMaintenance = "under_maintenance";
+	DegradedPerformance = "degraded_performance";
+	PartialOutage = "PartialOutage";
+	MajorOutage = "major_outage";
+	Unavailable = "";
+	
+}
+
+return StatusPageStatus
